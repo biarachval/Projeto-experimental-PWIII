b6d514fe7ee2da9da2e877d98af43fde8c482b69
biarachval <bia13sanches@gmail.com>
2021-06-18 20:12:26 -0300

    commit

---
+++ library/js/load-link.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    $('.nav-link').click(function(e) {
+        e.preventDefault()
+
+        let url = $(this).attr('href')
+        $('#conteudo').empty()
+        $('#conteudo').load(url)
+
+    })
+})
